a365b4999f2b0f7e122c35e4fd1bc36d82f6f2ba
Parth Vyas <parthvyas@Parths-MacBook-Air.local>
2021-10-29 06:43:42 +0530

    second commit

--- assembler.py
+++ assembler.py
@@ -1,2 +1,22 @@
+from typing import Counter
 import numpy as np
 import sys
+
+class PC:
+    instruction_mem = 0
+    count = 0
+
+#all registers from R0 to R31 with their binary strings
+register_dict = {
+
+}
+
+#encoding types of all 17 instructions (we will go with A, B, C and D), 4 types given in the doc
+type_dict = {
+
+}
+
+#we will go through the code in one iteration and see where the labels are then assign them with their line number 
+label_dict = {
+
+}
